652400d198a8fa128038916c01fbacef10b8a714
Amit Assaraf <amit@landa.app>
2020-01-14 02:25:21 +0200

    Support for more functions

--- src/pyprimetrust/client.py
+++ src/pyprimetrust/client.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
 from enum import Enum
 from typing import Tuple
+from uuid import uuid4
 
 from box import Box
 from requests import Session, Response
@@ -12,10 +14,13 @@ from .utils import require_connection
 
 class PrimeURLs(Enum):
     JWT_AUTH = '/auth/jwts'
-    CUSTODY_AGREEMENT_PREVIEW = '/agreement-previews'
+    CUSTODY_AGREEMENT_PREVIEW = '/v2/agreement-previews'
     CUSTODY_ACCOUNT = '/v2/accounts'
     CONTACTS = '/v2/contacts'
+    FUND_TRANSFER = '/v2/funds-transfers'
     FUND_TRANSFER_METHODS = '/v2/funds-transfer-methods'
+    CONTRIBUTIONS = '/v2/contributions'
+    DISBURSEMENTS = '/v2/disbursements'
 
 
 class PrimeClient(Session):
@@ -29,6 +34,8 @@ class PrimeClient(Session):
 
     def request(self, method, url, *args, **kwargs) -> Tuple[Box, Response]:
         url = urljoin(self._base_url, url)
+        if method == 'post':
+            self.headers.update('X-Request-ID', uuid4())
         response = super(PrimeClient, self).request(method, url, *args, **kwargs)
         return Box(response.json()), response
 
@@ -39,7 +46,7 @@ class PrimeClient(Session):
         return True
 
     @require_connection
-    def custody_account_agreement_preview(self, contact: Contact):
+    def custody_account_agreement_preview(self, contact: Contact) -> DataNode:
         data, http_response = self.post(PrimeURLs.CUSTODY_AGREEMENT_PREVIEW,
                                         data=RootDataNode(
                                             data=DataNode(
@@ -55,7 +62,7 @@ class PrimeClient(Session):
         return DataNode(**data.data.to_dict())
 
     @require_connection
-    def create_custody_account(self, contact: Contact):
+    def create_custody_account(self, contact: Contact) -> DataNode:
         data, http_response = self.post(PrimeURLs.CUSTODY_ACCOUNT,
                                         data=RootDataNode(
                                             data=DataNode(
@@ -71,7 +78,7 @@ class PrimeClient(Session):
         return DataNode(**data.data.to_dict())
 
     @require_connection
-    def create_entity_custody_account(self, contact: Contact):
+    def create_entity_custody_account(self, contact: Contact) -> DataNode:
         data, http_response = self.post(PrimeURLs.CUSTODY_ACCOUNT,
                                         data=RootDataNode(
                                             data=DataNode(
@@ -87,13 +94,13 @@ class PrimeClient(Session):
         return DataNode(**data.data.to_dict())
 
     @require_connection
-    def activate_custody_account(self, custody_account_id):
+    def activate_custody_account(self, custody_account_id: str) -> DataNode:
         data, http_response = self.post(
             urljoin(PrimeURLs.CUSTODY_ACCOUNT, f'/{custody_account_id}', f'/{self._environment}', f'/open'))
-        return data.data
+        return DataNode(**data.data.to_dict())
 
     @require_connection
-    def start_custody_kyc_process(self, custody_account_id, contact: Contact):
+    def start_custody_kyc_process(self, custody_account_id: str, contact: Contact) -> DataNode:
         data, http_response = self.post(PrimeURLs.CONTACTS,
                                         data=RootDataNode(
                                             data=DataNode(
@@ -107,7 +114,7 @@ class PrimeClient(Session):
         return DataNode(**data.data.to_dict())
 
     @require_connection
-    def get_custody_kyc_status(self, contact_id):
+    def get_custody_kyc_status(self, contact_id: str) -> RootListDataNode:
         data, http_response = self.get(PrimeURLs.CONTACTS,
                                        params={
                                            'filter[contact.id eq]': contact_id,
@@ -116,7 +123,7 @@ class PrimeClient(Session):
         return RootListDataNode(**data.to_dict())
 
     @require_connection
-    def add_fund_transfer_method(self, contact_id, transfer_method: FundTransferMethod):
+    def add_fund_transfer_method(self, contact_id: str, transfer_method: FundTransferMethod) -> DataNode:
         data, http_response = self.post(PrimeURLs.FUND_TRANSFER_METHODS,
                                         data=RootDataNode(
                                             data=DataNode(
@@ -128,3 +135,46 @@ class PrimeClient(Session):
                                             )
                                         ).to_json())
         return DataNode(**data.data.to_dict())
+
+    @require_connection
+    def deposit_funds(self, custody_account_id: str, fund_transfer_method_id: str, amount: Decimal) -> DataNode:
+        data, http_response = self.post(
+            PrimeURLs.CONTRIBUTIONS,
+            params={'include': 'funds-transfer'},
+            data=RootDataNode(
+                data=DataNode(
+                    type="contributions",
+                    attributes={
+                        "amount": amount,
+                        "funds-transfer-method-id": fund_transfer_method_id,
+                        "account-id": custody_account_id
+                    }
+                )
+            ).to_json())
+        return DataNode(**data.data.to_dict())
+
+    @require_connection
+    def withdraw_funds(self, custody_account_id: str, fund_transfer_method_id: str, amount: Decimal) -> DataNode:
+        data, http_response = self.post(
+            PrimeURLs.DISBURSEMENTS,
+            params={'include': 'funds-transfer,disbursement-authorization'},
+            data=RootDataNode(
+                data=DataNode(
+                    type="disbursements",
+                    attributes={
+                        "amount": amount,
+                        "funds-transfer-method-id": fund_transfer_method_id,
+                        "account-id": custody_account_id
+                    }
+                )
+            ).to_json())
+        return DataNode(**data.data.to_dict())
+
+    @require_connection
+    def get_funds_transfer_status(self, funds_transfer_id: str) -> RootListDataNode:
+        data, http_response = self.get(PrimeURLs.FUND_TRANSFER,
+                                       params={
+                                           'filter[id eq]': funds_transfer_id,
+                                           'include': 'contingent-holds'
+                                       })
+        return RootListDataNode(**data.to_dict())
