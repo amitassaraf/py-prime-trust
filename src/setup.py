0b7c2a7d5f623cfff3baf50eb7bd2f1f862e5a35
Amit Assaraf <amit@landa.app>
2020-01-25 02:58:55 +0200

    Ver bump

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='primetrust',
-    version='1.0.7',
+    version='1.0.8',
     private=True,
     description='API for primetrust',
     author='Amit Assaraf',
